f795e5154b302b2fd377239f4addf8cefa712274
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-11 15:55:25 +0200

    feat: add handleShowCart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header(){
+    const userProgressCtx = useContext(UserProgressContext);
+    const cartCtx = useContext(CartContext);
+
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+        return totalNumberOfItems + item.quantity;
+    }, 0);
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
+
     return(
         <header id="main-header">
             <div id="title">
@@ -9,7 +23,7 @@ export default function Header(){
                 <h1>ReactFood</h1>
             </div>
             <nav>
-                <Button textOnly>Cart [item]</Button>
+                <Button onClick={handleShowCart} textOnly>Cart [{totalCartItems}]</Button>
             </nav>
         </header>
     )
