922bd2c62db8847456e8ae2b7b2405502cf613e8
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-11 16:23:38 +0200

    feat: add CartItem component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import Modal from "./UI/Modal";
 import Button from "./UI/Button";
+import CartItem from "./CartItem";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
 import UserProgressContext from '../store/UserProgressContext';
@@ -23,9 +24,9 @@ export default function Cart(){
         <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
             <h2>Your Cart</h2>
             <ul>
-                {cartCtx.items.map((item) => <li key={item.id}>{item.name} - {item.quantity}</li>)}
             </ul>
             <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+                {cartCtx.items.map((item) => <CartItem onDecrease={() => cartCtx.removeItem(item.id)} onIncrease={() => cartCtx.addItem(item)} key={item.id} item={item}></CartItem>)}
             <p className="modal-actions">
                 <Button onClick={handleCloseCart} textOnly>Close</Button>
                 <Button>Go to Checkout</Button>
